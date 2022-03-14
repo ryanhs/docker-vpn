cef86f9c8d1275881e4b647b8b729c7267553bac
ryan hs <mr.ryansilalahi@gmail.com>
2022-03-14 09:13:06 +0700

    Update connectvpn.sh

--- bin/connectvpn.sh
+++ bin/connectvpn.sh
@@ -3,6 +3,8 @@ set -e
 set -x
 
 echo "connect to vpn..."
+
+# https://www.infradead.org/openconnect/ << protocols
 # echo $VPN_PASS | openconnect --disable-ipv6 --protocol=nc --os=linux $VPN_HOST -u $VPN_USER --passwd-on-stdin
 
 # iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
